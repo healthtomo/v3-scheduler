c402705eb7f20ce911471a05a295940a2bfea788
Alexander <alexanderdev.contact@gmail.com>
2024-09-20 12:59:18 -0400

    s

--- app/components/scheduler.editor.tsx
+++ app/components/scheduler.editor.tsx
@@ -3,30 +3,30 @@ import { SessionData } from "~/sessions";
 import FallBack from "./fallback";
 import Scheduler from "./nylas-react.client";
 import {
-  CustomIdentityRequestWrapperAccessToken,
-  CustomIdentityRequestWrapperProxy,
+    CustomIdentityRequestWrapperAccessToken,
+    CustomIdentityRequestWrapperProxy,
 } from "./scheduler.identity";
 
 type EditorProps = {
-  nylasClientId: string;
-  domain?: string;
-  queryParams?: EditorQueryParams;
-  userCreds?: SessionData;
-  origin?: string;
+    nylasClientId: string;
+    domain?: string;
+    queryParams?: EditorQueryParams;
+    userCreds?: SessionData;
+    origin?: string;
 };
 
 export type EditorQueryParams = {
-  configurationId: string;
-  requiresSlug: boolean;
-  accessType: AccessType;
-  email: string;
-  grantId: string;
+    configurationId: string;
+    requiresSlug: boolean;
+    accessType: AccessType;
+    email: string;
+    grantId: string;
 };
 
 export enum AccessType {
-  STANDARD = "standard",
-  ACCESS_TOKEN = "accessToken",
-  NONE = "none",
+    STANDARD = "standard",
+    ACCESS_TOKEN = "accessToken",
+    NONE = "none",
 }
 
 /**
@@ -44,100 +44,100 @@ export enum AccessType {
  */
 
 export default function NylasSchedulerEditor({
-  nylasClientId,
-  domain,
-  queryParams,
-  userCreds,
-  origin,
+    nylasClientId,
+    domain,
+    queryParams,
+    userCreds,
+    origin,
 }: EditorProps) {
-  const props = () => {
-    if (!queryParams) {
-      return undefined;
-    }
+    const props = () => {
+        if (!queryParams) {
+            return undefined;
+        }
 
-    let nylasApiRequest = undefined;
+        let nylasApiRequest = undefined;
 
-    /**
-     * This will capture the editor Auth mode. When you pass in accessType=AccessType.ACCESS_TOKEN or accessType=AccessType.None it will create a CustomIdentityWrapper
-     */
+        /**
+         * This will capture the editor Auth mode. When you pass in accessType=AccessType.ACCESS_TOKEN or accessType=AccessType.None it will create a CustomIdentityWrapper
+         */
 
-    if (queryParams.accessType === AccessType.ACCESS_TOKEN && userCreds) {
-      nylasApiRequest = new CustomIdentityRequestWrapperAccessToken({
-        accessToken: userCreds.accessToken,
-        email: userCreds?.email as string,
-        grantId: userCreds?.grantId as string,
-        domain,
-      });
-    }
+        if (queryParams.accessType === AccessType.ACCESS_TOKEN && userCreds) {
+            nylasApiRequest = new CustomIdentityRequestWrapperAccessToken({
+                accessToken: userCreds.accessToken,
+                email: userCreds?.email as string,
+                grantId: userCreds?.grantId as string,
+                domain,
+            });
+        }
 
-    if (queryParams.accessType === AccessType.NONE && userCreds) {
-      nylasApiRequest = new CustomIdentityRequestWrapperProxy({
-        email: userCreds?.email as string,
-        grantId: userCreds?.grantId as string,
-        domain: origin,
-      });
-    }
+        if (queryParams.accessType === AccessType.NONE && userCreds) {
+            nylasApiRequest = new CustomIdentityRequestWrapperProxy({
+                email: userCreds?.email as string,
+                grantId: userCreds?.grantId as string,
+                domain: origin,
+            });
+        }
 
-    return {
-      configurationId: queryParams.configurationId,
-      requiresSlug: queryParams.requiresSlug || false,
-      schedulerPreviewLink: queryParams.requiresSlug
-        ? `https://book.nylas.com/us/${nylasClientId}/{slug}`
-        : `${origin}/scheduler/{config.id}`,
-      ...(nylasApiRequest && { nylasApiRequest }),
+        return {
+            configurationId: queryParams.configurationId,
+            requiresSlug: queryParams.requiresSlug || false,
+            schedulerPreviewLink: queryParams.requiresSlug
+                ? `https://book.nylas.com/us/${nylasClientId}/{slug}`
+                : `${origin}/scheduler/{config.id}`,
+            ...(nylasApiRequest && { nylasApiRequest }),
+        };
     };
-  };
 
-  return (
-    <div className=" m-auto flex items-center justify-center h-full">
-      <ClientOnly fallback={<FallBack />}>
-        {() => {
-          return (
-            <Scheduler.NylasSchedulerEditor
-              eventOverrides={{
-                createButtonClick: async (e: unknown) => {
-                  console.log("Page created", e);
-                },
-                editButtonClick: async (e: unknown) => {
-                  console.log("Page Edited", e);
-                },
-                schedulerConfigChanged: async (e) => {
-                  console.log("Page changed", e);
-                  console.log(e.detail.config.id);
-                  //check for e.detail.id if it's present then it's saveChanges otherwise the page hasn't been created yet
-                },
-                deleteButtonClick: async (e) => {
-                  console.log("Page deleted", e);
-                },
-                previewButtonClicked: async (event, connector) => {
-                  event.preventDefault();
-                  console.log("Event", event);
-                  console.log("connector", connector);
-                },
-              }}
-              mode="app"
-              nylasSessionsConfig={{
-                clientId: nylasClientId, // Replace with your Nylas client ID from the previous
-                redirectUri: `${origin}/editor`,
-                domain: domain, // or 'https://api.eu.nylas.com/v3' for EU data center
-                hosted: true,
-                accessType: "offline",
-              }}
-              defaultSchedulerConfigState={{
-                selectedConfiguration: {
-                  requires_session_auth: false,
-                  scheduler: {
-                    // NOTE: Keep this path same for reschedule and cancel flow
-                    rescheduling_url: `${origin}/scheduler/reschedule/:booking_ref`,
-                    cancellation_url: `${origin}/scheduler/cancel/:booking_ref`,
-                  },
-                },
-              }}
-              {...props()}
-            ></Scheduler.NylasSchedulerEditor>
-          );
-        }}
-      </ClientOnly>
-    </div>
-  );
+    return (
+        <div className=" m-auto flex items-center justify-center h-full bg-color[#FBFBFB]">
+            <ClientOnly fallback={<FallBack />}>
+                {() => {
+                    return (
+                        <Scheduler.NylasSchedulerEditor
+                            eventOverrides={{
+                                createButtonClick: async (e: unknown) => {
+                                    console.log("Page created", e);
+                                },
+                                editButtonClick: async (e: unknown) => {
+                                    console.log("Page Edited", e);
+                                },
+                                schedulerConfigChanged: async (e) => {
+                                    console.log("Page changed", e);
+                                    console.log(e.detail.config.id);
+                                    //check for e.detail.id if it's present then it's saveChanges otherwise the page hasn't been created yet
+                                },
+                                deleteButtonClick: async (e) => {
+                                    console.log("Page deleted", e);
+                                },
+                                previewButtonClicked: async (event, connector) => {
+                                    event.preventDefault();
+                                    console.log("Event", event);
+                                    console.log("connector", connector);
+                                },
+                            }}
+                            mode="app"
+                            nylasSessionsConfig={{
+                                clientId: nylasClientId, // Replace with your Nylas client ID from the previous
+                                redirectUri: `${origin}/editor`,
+                                domain: domain, // or 'https://api.eu.nylas.com/v3' for EU data center
+                                hosted: true,
+                                accessType: "offline",
+                            }}
+                            defaultSchedulerConfigState={{
+                                selectedConfiguration: {
+                                    requires_session_auth: false,
+                                    scheduler: {
+                                        // NOTE: Keep this path same for reschedule and cancel flow
+                                        rescheduling_url: `${origin}/scheduler/reschedule/:booking_ref`,
+                                        cancellation_url: `${origin}/scheduler/cancel/:booking_ref`,
+                                    },
+                                },
+                            }}
+                            {...props()}
+                        ></Scheduler.NylasSchedulerEditor>
+                    );
+                }}
+            </ClientOnly>
+        </div>
+    );
 }
