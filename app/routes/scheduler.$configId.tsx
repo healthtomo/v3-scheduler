9ad66b63b4ad59e5d1991dbe95bd23fb7bbcc110
Alexander <alexanderdev.contact@gmail.com>
2024-09-26 22:02:25 -0400

    add title to page

--- app/routes/scheduler.$configId.tsx
+++ app/routes/scheduler.$configId.tsx
@@ -1,53 +1,62 @@
-import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import NylasCustomScheduler, {
-  SchedulerCustomQueryParams,
+    SchedulerCustomQueryParams,
 } from "~/components/scheduler";
 import configServer from "~/models/config.server";
 import sessionServer from "~/models/nylas/scheduler.server";
 import { parseQueryParams } from "~/models/utils/utils.server";
 
 type LoaderData = {
-  configurationId: string;
-  sessionId: string;
-  bookingInfo: SchedulerCustomQueryParams;
-  domain: string;
+    configurationId: string;
+    sessionId: string;
+    bookingInfo: SchedulerCustomQueryParams;
+    domain: string;
 };
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
-  const configurationId = params.configId;
+    const configurationId = params.configId;
 
-  const url = new URL(request.url);
+    const url = new URL(request.url);
 
-  const bookingInfo = parseQueryParams<SchedulerCustomQueryParams>(
-    url.searchParams,
-    ["name", "email"]
-  );
+    const bookingInfo = parseQueryParams<SchedulerCustomQueryParams>(
+        url.searchParams,
+        ["name", "email"]
+    );
 
-  if (!configurationId) {
-    return redirect("/error");
-  }
+    if (!configurationId) {
+        return redirect("/error");
+    }
 
-  const sessionId = await sessionServer.createSchedulerSession({
-    configurationId,
-    ttl: 30,
-  });
-  return json({
-    configurationId,
-    sessionId,
-    bookingInfo,
-    domain: configServer.API_ENDPOINT,
-  });
+    const sessionId = await sessionServer.createSchedulerSession({
+        configurationId,
+        ttl: 30,
+    });
+    return json({
+        configurationId,
+        sessionId,
+        bookingInfo,
+        domain: configServer.API_ENDPOINT,
+    });
 }
+
+export const meta: MetaFunction = () => {
+    return [
+        { title: "Scheduler | HealthTomo" },
+    ];
+};
+
+
 export default function Scheduler() {
-  const { configurationId, sessionId, bookingInfo, domain } =
-    useLoaderData<LoaderData>();
-  return (
-    <NylasCustomScheduler
-      configId={configurationId ?? ""}
-      sessionId={sessionId}
-      queryParams={bookingInfo}
-      domain={domain}
-    />
-  );
+
+    const { configurationId, sessionId, bookingInfo, domain } =
+        useLoaderData<LoaderData>();
+    return (
+        <NylasCustomScheduler
+            configId={configurationId ?? ""}
+            sessionId={sessionId}
+            queryParams={bookingInfo}
+            domain={domain}
+        />
+    );
 }
