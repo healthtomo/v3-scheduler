a3fbe7b198ad755c7a26fae495a8f7db6d16df44
Alexander <alexanderdev.contact@gmail.com>
2024-10-08 14:04:21 -0400

    add logging scheduler

--- app/routes/scheduler.webhook.ts
+++ app/routes/scheduler.webhook.ts
@@ -2,27 +2,30 @@ import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { logger } from "~/logger.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
+    const url = new URL(request.url);
 
-  const challenge = url.searchParams.get("challenge");
+    const challenge = url.searchParams.get("challenge");
 
-  if (!challenge) {
-    return new Response(null, {
-      status: 400,
+    if (!challenge) {
+        return new Response(null, {
+            status: 400,
+        });
+    }
+
+    return new Response(challenge, {
+        status: 200,
     });
-  }
-
-  return new Response(challenge, {
-    status: 200,
-  });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  const body = await request.json();
+    const body = await request.json();
+    console.log("Webhook logging");
+    console.log(body);
+    console.log(JSON.stringify(body, null, 2));
 
-  logger.info("Webhook received", { body });
+    logger.info("Webhook received", { body });
 
-  return new Response(null, {
-    status: 200,
-  });
+    return new Response(null, {
+        status: 200,
+    });
 }
