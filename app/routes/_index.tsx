da2970c0a5bb67e55705875e32d0c763bcade17d
healthtomo <haruna@gusplace.com>
2024-10-22 11:30:20 -0500

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="mx-auto h-6/6 max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="mx-auto max-w-lg text-center">
             <h2 className="text-3xl font-bold sm:text-4xl">
-              Nylas v3 Scheduler
+              Nylas v3 Scheduler Sandbox
             </h2>
 
             <p className="mt-4 text-gray-300">
